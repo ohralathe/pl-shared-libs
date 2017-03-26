c2a708d8fcc659d4c9fe50bd5d05f2c8d8d00035
ohralathe <voxuanhoan@gmail.com>
2017-03-26 17:01:51 +0700

    tetst

---
+++ src/libs/Base.groovy
@@ -0,0 +1,24 @@
+/**
+ * Created by hoanvo on 3/26/17.
+ */
+class Base implements Serializable {
+  def ctx
+  String orgName
+
+  Base(paramsMap) {
+    this.ctx = paramsMap.ctx
+    this.orgName = paramsMap.orgName
+  }
+
+  void build() {
+    this.ctx.println("Run the build steps from shared libs")
+  }
+
+  void deploy() {
+    if (this.orgName == 'ohralathe') {
+      this.ctx.println("Run the deploy steps from shared libs")
+    } else {
+      this.ctx.println("Don't run the deploy steps from shared libs")
+    }
+  }
+}
