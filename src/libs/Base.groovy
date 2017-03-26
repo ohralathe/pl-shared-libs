686ed8b34f5f98ec51bdc8661535aabe2006583a
ohralathe <voxuanhoan@gmail.com>
2017-03-26 18:07:55 +0700

    tetst

--- src/libs/Base.groovy
+++ src/libs/Base.groovy
@@ -16,14 +16,14 @@ class Base implements Serializable {
     this.ctx.println("Run the build steps from shared libs")
     this.ctx.println("""
       the info: ${this.ctx.env.BRANCH_NAME}
-      CHANGE_ID: ${this.ctx.CHANGE_ID}
-      CHANGE_URL: ${CHANGE_URL}
-CHANGE_TITLE: ${CHANGE_TITLE}
-CHANGE_AUTHOR: ${CHANGE_AUTHOR}
-CHANGE_AUTHOR_DISPLAY_NAME: ${CHANGE_AUTHOR_DISPLAY_NAME}
-CHANGE_AUTHOR_EMAIL: ${CHANGE_AUTHOR_EMAIL}
-CHANGE_TARGET: ${CHANGE_TARGET}
-WORKSPACE: ${WORKSPACE}
+      CHANGE_ID: ${this.ctx.env.CHANGE_ID}
+      CHANGE_URL: ${this.ctx.env.CHANGE_URL}
+      CHANGE_TITLE: ${this.ctx.env.CHANGE_TITLE}
+      CHANGE_AUTHOR: ${this.ctx.env.CHANGE_AUTHOR}
+      CHANGE_AUTHOR_DISPLAY_NAME: ${this.ctx.env.CHANGE_AUTHOR_DISPLAY_NAME}
+      CHANGE_AUTHOR_EMAIL: ${this.ctx.env.CHANGE_AUTHOR_EMAIL}
+      CHANGE_TARGET: ${this.ctx.env.CHANGE_TARGET}
+      WORKSPACE: ${this.ctx.env.WORKSPACE}
     """)
   }
 
